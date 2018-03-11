2232105c8c0781ec0d9dad2d28fc9ce24a3ce506
yifanxing <xing.yif@Husky.neu.edu>
2018-03-11 17:33:12 -0400

    making a blog on pythonanywhere

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yifanxing/Documents/Future/SideProjects/hello-django/virtualenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
